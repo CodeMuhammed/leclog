bc1b14429719d1c602576c3492e0d6e33db96b89
codemuhammed <codemuhammed@gmail.com>
2015-01-27 22:42:05 +0100

    commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var connect = require("connect");
 var serveStatic = require("serve-static");
 var app =connect();
 app.use(serveStatic('project-tiwit'));
-app.listen(7000);
+app.listen( 7000 || process.env.PORT);
 console.log("server running at localhost:7000");
