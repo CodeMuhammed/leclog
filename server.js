2be788801b857c60ee3c61838742320ff69fc10d
codemuhammed <codemuhammed@gmail.com>
2015-01-27 23:09:37 +0100

    changed port environment for server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var connect = require("connect");
 var serveStatic = require("serve-static");
 var app =connect();
 app.use(serveStatic('project-tiwit'));
-app.listen( 7000 || process.env.PORT);
+app.listen(process.env.PORT || 7000);
 console.log("server running at localhost:7000");
