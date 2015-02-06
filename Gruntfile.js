089e41206e57bae7ead39e1c8fc5d8b07d53e2b1
codemuhammed <codemuhammed@gmail.com>
2015-02-06 18:05:21 +0100

    tweaked the css a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,5 +46,5 @@ module.exports = function(grunt){
    grunt.loadNpmTasks('grunt-contrib-uglify');
    
    //default task(s)
-   grunt.registerTask("default" , ['jshint' , 'concat' , 'uglify']);
+   grunt.registerTask("default" , ['jshint']);
 };
